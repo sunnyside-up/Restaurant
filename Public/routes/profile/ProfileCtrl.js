6e983d0f968e83f634f8771f41a432fcc21a8a8b
skywindzz <skywindzz@yahoo.com>
2015-06-05 10:36:53 -0600

    june5 10:36

--- Public/routes/profile/ProfileCtrl.js
+++ Public/routes/profile/ProfileCtrl.js
@@ -3,6 +3,12 @@
 		.controller('ProfileCtrl', ['$scope', function($scope){
 			var PR = this;
 			
+				
+		this.getRestaruantInfo = function (){
+			ProfileService.getRestaruantInfo().then(function(data){
+				PR.restaruantInfo = data;
+			})
+		}
 			
 		}]); //end of controller
 })();
--- Public/routes/profile/ProfileService.js
+++ Public/routes/profile/ProfileService.js
@@ -2,7 +2,18 @@
 	angular.module('MRE')
 		.service('ProfileService', ['$q', '$http', function($q, $http){
 
-			
+		//get restaruant info for profile page
+		this.getRestaruantInfo = function() {
+			var deferred = $q.defer();
+			$http({
+				method : 'GET',
+				url :          //need endpoint here
+			}).then(function(response){
+				deferred.resolve(response.data)
+			})
+				return deferred.promise;
+		}	
+		
 			
 		}]); //end of service
 })();
