6b7837b5741504fbebc6ba2e689c8134ea2c140b
mikestarks91 <mike.j.starks@gmail.com>
2015-06-08 11:26:31 -0600

    last commit for a while...

--- ClientPublic/app.js
+++ ClientPublic/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('MRE', ['ui.router','ui.bootstrap']);
+var app = angular.module('MRE', ['ui.router', 'ui.bootstrap']);
 
 
 app.config(function($stateProvider, $urlRouterProvider){
