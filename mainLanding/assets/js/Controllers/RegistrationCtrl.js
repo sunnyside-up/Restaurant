88645c81f5061daa4c35c7a50ab0c09bd8f06bb3
allancheow <allan.cheow132@gmail.com>
2015-06-09 16:02:17 -0600

    updated laning page with working modal form only

--- mainLanding/assets/js/Controllers/RegistrationCtrl.js
+++ mainLanding/assets/js/Controllers/RegistrationCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('MRE');
 
-app.controller('BusinessLoginCtrl', function($scope, $log, $modalInstance) {
+app.controller('RegistrationCtrl', function($scope, $log, $modalInstance) {
 
 	$scope.cancel = function () {
 	    $modalInstance.dismiss('cancel');
