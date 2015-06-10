2b67580357edf7206ca9c491bbedd2517a9f2f49
mikestarks91 <mike.j.starks@gmail.com>
2015-06-10 17:25:26 -0600

    modal changed

--- ClientPublic/controllers/appSettingsCtrl.js
+++ ClientPublic/controllers/appSettingsCtrl.js
@@ -1,6 +1,18 @@
 var app = angular.module('MRE');
 
-app.controller('AppSettingsCtrl', function($scope) {
+app.controller('AppSettingsCtrl', function($scope, $log, $modalInstance) {
 	var AS = this;
+
+	$scope.checkbox = {
+		notes: true
+	};
+
+	$scope.saveForm = function() {
+
+	};
+
+	$scope.cancel = function() {
+		$modalInstance.dismiss('cancel');
+	};
 
 });
--- ClientPublic/controllers/mainCtrl.js
+++ ClientPublic/controllers/mainCtrl.js
@@ -1,6 +1,24 @@
 var app = angular.module('MRE');
 
-app.controller('MainCtrl', function($scope) {
-	var Main = this;
+app.controller('MainCtrl', function($scope, $log, $modal) {
+	// Fixes modal caching problem (not sure how this works)
+	var randStr = '?bust=' + Math.random().toString(36).slice(2);
+
+	$scope.animationsEnabled = true;
+
+	$scope.openAppSettings = function(size) {
+		var modalInstance = $modal.open({
+			animation: $scope.animationsEnabled,
+			templateUrl: 'views/AppSettingsModal.html'+randStr,
+			controller: 'AppSettingsCtrl',
+			size: size
+		});
+
+		modalInstance.result.then(function(selectedItem) {
+			$scope.selected = selectedItem;
+		}, function() {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+	};
 
 });
