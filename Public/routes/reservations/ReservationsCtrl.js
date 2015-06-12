cccf256a8f86536cc3107bc9c4207966f6f613af
Edgar <ew1773@gmail.com>
2015-06-12 11:14:22 -0600

    Reservation page finnished until data is avalible

--- Public/routes/reservations/ReservationsCtrl.js
+++ Public/routes/reservations/ReservationsCtrl.js
@@ -2,44 +2,24 @@
 	angular.module('MRE')
 		.controller('ReservationsCtrl', ['$scope', "ReservationsService",'$modal', '$log',function($scope, ReservationsService, $modal, $log){
 			var RE = this;
-			
- $scope.items = ['item1', 'item2', 'item3'];
-  
-    $scope.animationsEnabled = true;
-  
-    RE.open = function (size) {
-  
-      var modalInstance = $modal.open({
-        animation: RE.animationsEnabled,
-        templateUrl: 'routes/reservations/Modal/modal.html',
-        controllerAs: 'REModalCtrl as REM',
-        size: size,
-        resolve: {
-          items: function () {
-            return REM.items;
-          }
-        }
-      });
+		
+                 RE.reservations = ReservationsService.getReservations();
+                 RE.reservations = [];
+		
+				 RE.reservationModal = function(size) {
+                    console.log('hello');
+                    var modalInstance = $modal.open({
+                        animation: $scope.animationsEnabled,
+                        templateUrl: 'routes/reservations/Modal/reservationModal.html?bust=' + Math.random().toString(36).slice(2),
+                        controller: 'ReservationModalCtrl as RMC',
+                        size: size
+                    });
 
-  
-  // Please note that $modalInstance represents a modal window (instance) dependency.
-  // It is not the same as the $modal service used above.
-
-  
-    $scope.items = items;
-    $scope.selected = {
-      item: $scope.items[0]
-    };
-  
-    $scope.ok = function () {
-      $modalInstance.close($scope.selected.item);
-    };
-  
-    $scope.cancel = function () {
-      $modalInstance.dismiss('cancel');
-    };
-  					
-			
-			RE.reservations = ReservationsService.getReservations();
-	 //end of controller
-    });
+                    modalInstance.result.then(function(selectedItem) {
+                        $scope.selected = selectedItem;
+                    }, function() {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    });
+                };
+		}]);	//end of controller	
+})();
