c40b0ba5a2d8a2a748e83c986680c1f8775553bb
mikestarks91 <mike.j.starks@gmail.com>
2015-06-22 15:30:59 -0600

    now pulling user data from server for profile

--- ClientPublic/controllers/editProfileCtrl.js
+++ ClientPublic/controllers/editProfileCtrl.js
@@ -1,18 +1,7 @@
 var app = angular.module('MRE');
 
-app.controller('EditProfileCtrl', function($scope, $log, $modalInstance, DashboardService) {
-	var AS = this;
-
-	$scope.checkbox = {
-		notes: true
-	};
-
-	$scope.saveForm = function() {
-
-	};
-
-	$scope.cancel = function() {
-		$modalInstance.dismiss('cancel');
-	};
+app.controller('EditProfileCtrl', function($scope, $log, $modalInstance, DashboardService, getClient) {
+	var EP = this;
+	console.log(getClient);
 
 });
--- ClientPublic/controllers/profileCtrl.js
+++ ClientPublic/controllers/profileCtrl.js
@@ -1,23 +1,38 @@
-var app = angular.module('MRE');
+(function() {
+	var app = angular.module('MRE');
 
-app.controller('CustProfileCtrl', function($scope, UserService, getClient2) {
+	app.controller('CustProfileCtrl', function($scope, UserService, getClient2, $modal, $log) {
 
-	var CP = this;
+		var CP = this;
 
-	$scope.user = getClient2;
-	$scope.editUser = function(size) {
-			var modalInstance = $modal.open({
-				animation: $scope.animationsEnabled,
-				templateUrl: 'views/EditProfileModal.html'+randStr,
-				controller: 'EditProfileCtrl',
-				size: size
-			});
+		var randStr = '?bust=' + Math.random().toString(36).slice(2);
 
-			modalInstance.result.then(function(selectedItem) {
-				$scope.selected = selectedItem;
-			}, function() {
-				$log.info('Modal dismissed at: ' + new Date());
-			})
-		}
-	
-});
+
+		console.log(getClient2);
+
+		CP.user = getClient2;
+
+		console.log(CP.user);
+
+		CP.editUser = function(size) {
+				var modalInstance = $modal.open({
+					animation: $scope.animationsEnabled,
+					templateUrl: 'views/EditProfileModal.html'+randStr,
+					controller: 'EditProfileCtrl',
+					resolve: {
+						getClient: function() {
+							return getClient2;
+						}
+					},
+					size: size
+				});
+
+				modalInstance.result.then(function(selectedItem) {
+					$scope.selected = selectedItem;
+				}, function() {
+					$log.info('Modal dismissed at: ' + new Date());
+				})
+			}
+		
+	});
+})();
